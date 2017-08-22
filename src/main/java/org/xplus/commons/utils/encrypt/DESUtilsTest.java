7446b3231086ed4bdd30a8614e4b9a5ba5a233c4
Administrator <Administrator@AK47-PC>
2017-08-22 23:27:06 +0800

    update

---
+++ src/main/java/org/xplus/commons/utils/encrypt/DESUtilsTest.java
@@ -0,0 +1,19 @@
+package org.xplus.commons.utils.encrypt;
+
+import junit.framework.TestCase;
+
+public class DESUtilsTest extends TestCase {
+
+	public void testHexString2Bytes() {
+		fail("Not yet implemented");
+	}
+
+	public void testEncrypt() {
+		fail("Not yet implemented");
+	}
+
+	public void testDecrypt() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/org/xplus/commons/utils/math/AbsUtilsTest.java
@@ -0,0 +1,7 @@
+package org.xplus.commons.utils.math;
+
+import junit.framework.TestCase;
+
+public class AbsUtilsTest extends TestCase {
+
+}
