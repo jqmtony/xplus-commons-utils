071bc1b97c635fc885040ca4a0434cb6df60dd26
jqmtony <jqmtony@NB-T440s>
2018-07-16 10:59:16 +0800

    更新

---
+++ src/main/java/org/xplus/commons/utils/DefConstants.java
@@ -0,0 +1,27 @@
+package org.xplus.commons.utils;
+
+/**
+ * 
+ * @author jqmtony
+ *
+ */
+public final class DefConstants {
+
+	//日期时间格式化定义
+	/**  */
+	public static final String DATE_FORMAT_DATEONLY = "yyyy-MM-dd";
+	public static final String DATE_FORMAT_DATEONLY_SHORT = "yyyyMMdd";
+	/** */
+	public static final String DATE_FORMAT_DATETIME = "yyyy-MM-dd HH:mm:ss";
+	/** */
+	public static final String DATE_TIME0 = "yyyy-MM-dd HH:mm";
+	/** */
+	public static final String DATE_TIME1 = "MM-dd";
+	/** */
+	public static final String DATE_TIME_MONTH = "MM-dd HH:mm:ss";
+	/** */
+	public static final String DATE_TIME_MILL_HOUR_MIN_MILL = "HH:mm:ss";
+	/** */
+	public static final String T_HOUR = "yyyy-MM-dd'T'HH:mm:ss";
+
+}
